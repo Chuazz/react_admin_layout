911f56bfd62c10f56a209b47303a0bc3d5933ce4
SơnNV <sonnv1912@gmail.com>
2023-07-22 12:01:18 +0700

    chore: update new version storybook, remove prettierrc

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,20 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
+
 const config: StorybookConfig = {
-	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-	addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: {
-		name: "@storybook/react-vite",
+		name: '@storybook/react-vite',
 		options: {},
 	},
 	docs: {
-		autodocs: "tag",
+		autodocs: 'tag',
 	},
 	typescript: {
-		reactDocgen: "react-docgen",
+		reactDocgen: 'react-docgen',
 		skipBabel: true,
 		check: false,
 	},
 };
+
 export default config;
